b67b1093a66874bad8e67fa90e165628f224ccfb
alex777scorpion <46317819+alex777scorpion@users.noreply.github.com>
2019-04-06 06:51:02 +0300

    Начальная версия

---
+++ src/Text.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './App.css';
+import './App';
+class App extends Component {
+    render() {
+        return <input type="name" className="Center" />
+        }
+    }
+
+
+export default App;
